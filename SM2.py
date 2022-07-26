eefc4c3df50339ea9f073c548ca4fab5ff83b844
nadphwr <105400084+nadphwr@users.noreply.github.com>
2022-07-26 14:37:57 +0800

    Add files via upload

--- SM2.py
+++ SM2.py
@@ -10,7 +10,7 @@ import random
 import time
 import math
 import numpy as np
-from pysmx.SM3 import digest as sm3
+from _sm3 import sm3
  
 # 小素数列表，加快判断素数速度
 small_primes = np.array([2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41,
@@ -28,35 +28,46 @@ small_primes = np.array([2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41,
  
  
 def is_prime(num):
-    # 排除0,1和负数
     if num < 2:
         return False
-    # 排除小素数的倍数
     for prime in small_primes:
         if num % prime == 0:
             return False
-    # 未分辨出来的大整数用rabin算法判断
-    return rabin_miller(num)
+    return rabin_miller(num,10)
  
  
-def rabin_miller(num):
-    s = num - 1
-    t = 0
-    while s & 1 == 0:
-        s >>= 1
-        t += 1
-    for trials in range(5):
-        a = random.randrange(2, num - 1)
-        v = pow(a, s, num)
-        if v != 1:
-            i = 0
-            while v != (num - 1):
-                if i == t - 1:
-                    return False
+def rabin_miller(n,times):
+    judge=0
+    for j in range(times):
+        a=random.randint(1,n-1)
+        #print(a)
+        N=n-1
+        k=0
+        while True:
+            if N%2 !=0:
+                m=N
+                break
+            N=N//2
+            k+=1    
+        b=pow(a,m,n)
+        #print(b)
+        flag=0
+        if b%n==1:
+            judge+=1
+        else:
+            for i in range(k):
+                if b%n==n-1:
+                    judge+=1
+                    flag=1
+                    break
                 else:
-                    i = i + 1
-                    v = v * v % num
-    return True
+                    b=pow(b,2,n)
+            if flag==0:
+                judge+=0
+    if judge>=0.25*times:
+        return True
+    else:
+        return False
  
  
 # 将字节转换为int
@@ -116,11 +127,7 @@ def get_(a, b):
  
 # 求乘法逆元
 def get_inverse(a, p):
-    # return pow(a, p-2, p) # 效率较低、n倍点的时候两种计算方法结果会有不同
-    if gcd(a, p) == 1:
-        x, y = get_(a, p)
-        return x % p
-    return 1
+    return pow(a, p-2, p) 
  
  
 def get_cpu_time():
@@ -136,7 +143,7 @@ def KDF(Z, klen):
     ksize = klen >> 3
     K = bytearray()
     for ct in range(1, math.ceil(ksize / HASH_SIZE) + 1):
-        K.extend(sm3(Z + to_byte(ct, 4)))
+        K+=bytearray(sm3(str(to_int(Z + to_byte(ct, 4)))).encode())
     return K[:ksize]
  
  
@@ -503,7 +510,18 @@ PARA_SIZE = 32  # 参数长度（字节）
 HASH_SIZE = 32  # sm3输出256位（32字节）
 KEY_LEN = 128  # 默认密钥位数
  
- 
+# SM2示例中的椭圆曲线系统参数
+def demo_para():
+    p = 0x8542D69E4C044F18E8B92435BF6FF7DE457283915C45517D722EDB8B08F1DFC3
+    a = 0x787968B4FA32C3FD2417842E73BBFEFF2F3C848B6831D7E0EC65228B3937E498
+    b = 0x63E4C6D3B23B0C849CF84241484BFE48F61D59A5B16BA06E6E12D1DA27C5249A
+    xG = 0x421DEBD61B62EAB6746434EBC3CC315E32220B3BADD50BDC4C4E6C147FEDD43D
+    yG = 0x0680512BCBB42C07D47349D2153B70C4E5D7FDFCBFA36EA1A85841B9E46E09A2
+    n = 0x8542D69E4C044F18E8B92435BF6FF7DD297720630485628D5AE74EE7C32E79B7
+    G = (xG, yG)
+    h = 1
+    return p, a, b, n, G, h
+
 # SM2类继承ECC
 class SM2(ECC):
     # 默认使用SM2推荐曲线参数
@@ -582,7 +600,7 @@ class SM2(ECC):
                 save = True
         entlen = get_bit_num(ID)
         ENTL = to_byte(entlen, 2)
-        Z = sm3(join_bytes([ENTL, ID, self.a, self.b, *self.G, *P]))
+        Z = sm3(str(to_int(join_bytes([ENTL, ID, self.a, self.b, *self.G, *P]))))
         if save:  # 保存自身Z值
             self.Z = Z
         return Z
@@ -594,7 +612,7 @@ class SM2(ECC):
     def sign(self, M, k=None, outbytes=True, dohash=True):
         if dohash:
             M_ = join_bytes([self.get_Z(), M])
-            e = to_int(sm3(M_))
+            e = to_int(bytearray(sm3(str(to_int(M_))).encode()))
         else:
             e = to_int(to_byte(M))
         while True:
@@ -633,7 +651,7 @@ class SM2(ECC):
             return False
         if dohash:
             M_ = join_bytes([self.get_Z(IDA, PA), M])
-            e = to_int(sm3(M_))
+            e = to_int(bytearray(sm3(str(to_int(M_))).encode()))
         else:
             e = to_int(to_byte(M))
         t = (r + s) % self.n
@@ -687,9 +705,9 @@ class SM2(ECC):
         if not option:
             return True, (RB, KB)
         # B8、B10（可选部分）
-        tmp = join_bytes([yV, sm3(join_bytes([xV, ZA, ZB, x1, y1, x2, y2]))])
-        SB = sm3(join_bytes([2, tmp]))
-        S2 = sm3(join_bytes([3, tmp]))
+        tmp = join_bytes([yV, sm3(str(to_int(join_bytes([xV, ZA, ZB, x1, y1, x2, y2]))))])
+        SB = sm3(str(to_int(join_bytes([2, tmp]))))
+        S2 = sm3(str(to_int(join_bytes([3, tmp]))))
         return True, (RB, KB, SB, S2)
  
     # A 协商确认
@@ -720,11 +738,11 @@ class SM2(ECC):
         if not option or not SB:
             return True, KA
         # A9-A10（可选部分）
-        tmp = join_bytes([yU, sm3(join_bytes([xU, ZA, ZB, x1, y1, x2, y2]))])
-        S1 = sm3(join_bytes([2, tmp]))
+        tmp = join_bytes([yU, sm3(str(to_int(join_bytes([xU, ZA, ZB, x1, y1, x2, y2]))))])
+        S1 = sm3(str(to_int(join_bytes([2, tmp]))))
         if S1 != SB:
             return False, 'S1 != SB'
-        SA = sm3(join_bytes([3, tmp]))
+        SA = sm3(str(to_int(join_bytes([3, tmp]))))
         return True, (KA, SA)
  
     # B 协商确认（可选部分）
@@ -756,7 +774,7 @@ class SM2(ECC):
         # C1 = to_byte(self.multiply(k, self.G), self.keysize) # (x1, y1)
         C1 = to_byte(self.Jacb_multiply(k, self.G), self.keysize)  # (x1, y1)
         C2 = to_byte(to_int(M) ^ t, klen >> 3)
-        C3 = sm3(join_bytes([x2, M, y2]))
+        C3 = sm3(str(to_int(join_bytes([x2, M, y2]))))
         return True, join_bytes([C1, C2, C3])
  
     # 解密
@@ -779,199 +797,14 @@ class SM2(ECC):
             return False, 't为全0比特串'
         C2 = C[self.keysize << 1:-HASH_SIZE]
         M = to_byte(to_int(C2) ^ t, klen >> 3)
-        u = sm3(join_bytes([x2, M, y2]))
+        u = sm3(str(to_int(join_bytes([x2, M, y2]))))
         C3 = C[-HASH_SIZE:]
         if u != C3:
             return False, 'u != C3'
         return True, M
  
  
-# 最简单的ECDH正确性测试
-def test_ECDH(verify=False):
-    time_1 = get_cpu_time()
-    sm2 = SM2(genkeypair=False)
-    # A、B双方生成公、私钥
-    dA, PA = sm2.gen_keypair()
-    dB, PB = sm2.gen_keypair()
-    # 验证ECC系统参数和公钥
-    if verify:
-        if not sm2.para_valid():
-            print('椭圆曲线系统参数未通过验证：%s' % sm2.error)
-            return
-        if not sm2.pk_valid(PA):
-            print('PA未通过验证：%s' % sm2.error)
-            return
-        if not sm2.pk_valid(PB):
-            print('PB未通过验证：%s' % sm2.error)
-            return
- 
-    # A将PA传给B，B将PB传给A
- 
-    # A、B双方计算密钥
-    QA = sm2.Jacb_multiply(dA, PB)
-    KA = KDF(to_byte(QA), KEY_LEN)
-    QB = sm2.Jacb_multiply(dB, PA)
-    KB = KDF(to_byte(QB), KEY_LEN)
-    time_2 = get_cpu_time()
-    print('ECDH密钥协商完毕，耗时%.2f ms' % ((time_2 - time_1) * 1000))
-    print('KA == KB?: %s, value: 0x%s, len: %d' % (KA == KB, KA.hex(), len(KA) << 3))
- 
- 
-# SM2密钥协商测试
-def test_SM2_agreement(option=False):
-    time_1 = get_cpu_time()
-    # A、B双方初始化
-    sm2_A = SM2(ID='Alice')
-    sm2_B = SM2(ID='Bob')
-    # A、B均掌握对方的公钥和ID
-    PA, IDA = sm2_A.pk, sm2_A.ID
-    PB, IDB = sm2_B.pk, sm2_B.ID
- 
-    # A 发起协商
-    rA, RA = sm2_A.agreement_initiate()
-    # A将RA发送给B
- 
-    # B 响应协商
-    res, content = sm2_B.agreement_response(RA, PA, IDA, option)
-    if not res:
-        print('B报告协商错误：', content)
-        return
-    if option:
-        RB, KB, SB, S2 = content
-    else:
-        RB, KB = content
-        SB = None
-    # B将RB、(选项SB)发送给A
- 
-    # A 协商确认
-    res, content = sm2_A.agreement_confirm(rA, RA, RB, PB, IDB, SB, option)
-    if not res:
-        print('A报告协商错误：', content)
-        return
-    if option:
-        KA, SA = content
-    else:
-        KA = content
- 
-    if option:
-        # A将(选项SA)发送给B
-        # B 协商确认
-        res, content = sm2_B.agreement_confirm2(S2, SA)
-        if not res:
-            print('B报告协商错误：', content)
-            return
-    time_2 = get_cpu_time()
-    print('SM2密钥协商完毕，耗时%.2f ms' % ((time_2 - time_1) * 1000))
-    print('KA == KB?: %s, value: 0x%s, len: %d' % (KA == KB, KA.hex(), len(KA) << 3))
- 
- 
-# SM2示例中的椭圆曲线系统参数
-def demo_para():
-    p = 0x8542D69E4C044F18E8B92435BF6FF7DE457283915C45517D722EDB8B08F1DFC3
-    a = 0x787968B4FA32C3FD2417842E73BBFEFF2F3C848B6831D7E0EC65228B3937E498
-    b = 0x63E4C6D3B23B0C849CF84241484BFE48F61D59A5B16BA06E6E12D1DA27C5249A
-    xG = 0x421DEBD61B62EAB6746434EBC3CC315E32220B3BADD50BDC4C4E6C147FEDD43D
-    yG = 0x0680512BCBB42C07D47349D2153B70C4E5D7FDFCBFA36EA1A85841B9E46E09A2
-    n = 0x8542D69E4C044F18E8B92435BF6FF7DD297720630485628D5AE74EE7C32E79B7
-    G = (xG, yG)
-    h = 1
-    return p, a, b, n, G, h
- 
- 
-# SM2数字签名与验证测试
-# SM2第2部分 A.1 A.2
-def test_signature():
-    IDA = 'ALICE123@YAHOO.COM'
-    M = 'message digest'
-    dA = 0x128B2FA8BD433C6C068C8D803DFF79792A519A55171B1B650C23661D15897263
-    xA = 0x0AE4C7798AA0F119471BEE11825BE46202BB79E2A5844495E97C04FF4DF2548A
-    yA = 0x7C0240F88F1CD4E16352A73C17B7F16F07353E53A176D684A9FE0C6BB798E857
-    PA = (xA, yA)
-    k = 0x6CB28D99385C175C94F94E934817663FC176D925DD72B727260DBAAE1FB2F96F
- 
-    # A、B双方初始化
-    sm2_A = SM2(*demo_para(), IDA, dA, PA)
-    sm2_B = SM2(*demo_para())
- 
-    time_1 = get_cpu_time()
-    # A对消息M进行签名
-    sig = sm2_A.sign(M, k)
- 
-    # A将消息M签名(r, s)发送给B
- 
-    # B对消息M签名进行验证
-    res = sm2_B.verify(M, sig, IDA, PA)
-    time_2 = get_cpu_time()
-    print('SM2签名、验证完毕，耗时%.2f ms' % ((time_2 - time_1) * 1000))
-    print('结果：%s，R值：%s' % (res, sig[:sm2_A.keysize].hex()))
-    # 验证通过，输出的r值(40f1ec59f793d9f49e09dcef49130d4194f79fb1eed2caa55bacdb49c4e755d1)与SM2第2部分 A.2中的结果一致
- 
- 
-# SM2密钥协商测试2
-# SM2第3部分 A.1 A.2
-def test_SM2_agreement2(option=False):
-    IDA = 'ALICE123@YAHOO.COM'
-    IDB = 'BILL456@YAHOO.COM'
-    dA = 0x6FCBA2EF9AE0AB902BC3BDE3FF915D44BA4CC78F88E2F8E7F8996D3B8CCEEDEE
-    xA = 0x3099093BF3C137D8FCBBCDF4A2AE50F3B0F216C3122D79425FE03A45DBFE1655
-    yA = 0x3DF79E8DAC1CF0ECBAA2F2B49D51A4B387F2EFAF482339086A27A8E05BAED98B
-    PA = (xA, yA)
-    dB = 0x5E35D7D3F3C54DBAC72E61819E730B019A84208CA3A35E4C2E353DFCCB2A3B53
-    xB = 0x245493D446C38D8CC0F118374690E7DF633A8A4BFB3329B5ECE604B2B4F37F43
-    yB = 0x53C0869F4B9E17773DE68FEC45E14904E0DEA45BF6CECF9918C85EA047C60A4C
-    PB = (xB, yB)
-    rA = 0x83A2C9C8B96E5AF70BD480B472409A9A327257F1EBB73F5B073354B248668563
-    x1 = 0x6CB5633816F4DD560B1DEC458310CBCC6856C09505324A6D23150C408F162BF0
-    y1 = 0x0D6FCF62F1036C0A1B6DACCF57399223A65F7D7BF2D9637E5BBBEB857961BF1A
-    RA = (x1, y1)
-    rB = 0x33FE21940342161C55619C4A0C060293D543C80AF19748CE176D83477DE71C80
-    x2 = 0x1799B2A2C778295300D9A2325C686129B8F2B5337B3DCF4514E8BBC19D900EE5
-    y2 = 0x54C9288C82733EFDF7808AE7F27D0E732F7C73A7D9AC98B7D8740A91D0DB3CF4
-    RB = (x2, y2)
- 
-    time_1 = get_cpu_time()
-    # A、B双方初始化
-    sm2_A = SM2(*demo_para(), IDA, dA, PA)
-    sm2_B = SM2(*demo_para(), IDB, dB, PB)
- 
-    # A 发起协商
-    # A生成rA, RA，将RA发送给B
- 
-    # B 响应协商
-    res, content = sm2_B.agreement_response(RA, PA, IDA, option, rB, RB)
-    if not res:
-        print('B报告协商错误：', content)
-        return
-    if option:
-        RB, KB, SB, S2 = content
-    else:
-        RB, KB = content
-        SB = None
-    # B将RB、(选项SB)发送给A
- 
-    # A 协商确认
-    res, content = sm2_A.agreement_confirm(rA, RA, RB, PB, IDB, SB, option)
-    if not res:
-        print('A报告协商错误：', content)
-        return
-    if option:
-        KA, SA = content
-    else:
-        KA = content
- 
-    if option:
-        # A将(选项SA)发送给B
-        # B 协商确认
-        res, content = sm2_B.agreement_confirm2(S2, SA)
-        if not res:
-            print('B报告协商错误：', content)
-            return
-    time_2 = get_cpu_time()
-    print('SM2密钥协商完毕，耗时%.2f ms' % ((time_2 - time_1) * 1000))
-    print('KA == KB?: %s, value: 0x%s, len: %d' % (KA == KB, KA.hex(), len(KA) << 3))
-    # 协商成功，输出的密钥(55b0ac62a6b927ba23703832c853ded4)与SM2第3部分 A.2中的结果一致
- 
- 
+
 # SM2加解密测试
 # SM2第4部分 A.1 A.2
 def test_encryption():
@@ -1007,9 +840,4 @@ def test_encryption():
  
  
 if __name__ == "__main__":
-    test_ECDH()
-    test_SM2_agreement(True)
-    # 可复现SM2文档中的示例结果
-    test_signature()
-    test_SM2_agreement2(True)
     test_encryption()
